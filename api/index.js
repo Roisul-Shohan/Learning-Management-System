b0412cfda3fad7ebc76f950ab757584c98c58413
Roisul-Shohan <roisul192@gmail.com>
2025-12-09 22:56:28 +0600

    Commit requested by user

--- api/index.js
+++ api/index.js
@@ -1,3 +1,16 @@
+console.log('API function starting...');
+
+const { connectDB } = require('../config/db');
+
+connectDB().then(() => {
+  console.log('Database connected successfully');
+  const app = require('../app');
+  console.log('App loaded successfully');
+  module.exports = app;
+}).catch(err => {
+  console.error('Database connection failed:', err);
+  process.exit(1);
+});
 const app = require('../app');
 
 module.exports = app;
